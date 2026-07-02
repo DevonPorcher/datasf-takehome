6e54784a366b715338a0bcf6c7b60a3f5bdfbce3
DevonPorcher <devonporcher@gmail.com>
2026-07-01 17:18:44 -0700

    clean up comments. add year_type accepted_values check to staging. add guard for masking unset.

--- dbt/macros/create_leadership_masking_policy.sql
+++ dbt/macros/create_leadership_masking_policy.sql
@@ -2,10 +2,22 @@
     {% set policy_name = target.database ~ '.' ~ generate_schema_name('analytics_finance', none) ~ '.leadership_masking_policy' %}
     {% set table_name = ref('employee_compensation') %}
 
-    {% set drop_sql %}
-        ALTER TABLE {{ table_name }} MODIFY COLUMN {{ column_name }} UNSET MASKING POLICY;
-    {% endset %}
-    {% do run_query(drop_sql) %}
+    {% if execute %}
+        {% set check_sql %}
+            SELECT COUNT(*) AS cnt
+            FROM {{ target.database }}.INFORMATION_SCHEMA.POLICY_REFERENCES
+            WHERE REF_ENTITY_NAME = '{{ table_name.identifier }}'
+            AND REF_COLUMN_NAME = '{{ column_name }}'
+            AND POLICY_KIND = 'MASKING_POLICY'
+        {% endset %}
+        {% set results = run_query(check_sql) %}
+        {% if results.rows[0][0] > 0 %}
+            {% set drop_sql %}
+                ALTER TABLE {{ table_name }} MODIFY COLUMN {{ column_name }} UNSET MASKING POLICY;
+            {% endset %}
+            {% do run_query(drop_sql) %}
+        {% endif %}
+    {% endif %}
 
     {% set create_sql %}
         CREATE OR REPLACE MASKING POLICY {{ policy_name }}
